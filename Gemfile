e1a3b53a10acabb622e0b2df03e8af78d194f888
velijoze <velijoze@outlook.com>
2017-03-14 08:15:01 +0000

    Photo Challenge

--- Gemfile
+++ Gemfile
@@ -52,3 +52,5 @@ gem 'devise', '>=3.2.4'
 gem "geocoder", "1.3.4"
 
 gem "figaro", ">= 1.0.0"
+
+gem 'carrierwave', '~> 1.0'
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'places#index'
   resources :places do
     resources :comments, only: :create
+    resources :photos, only: :create
   end
   
   # The priority is based upon order of creation: first created -> highest priority.
